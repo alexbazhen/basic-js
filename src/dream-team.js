5575847e218e31b2077d65bce1f0e1a45bd023f1
Alexander Bazhenov <alexbazhen@yandex.ru>
2022-05-02 01:43:12 +0300

    feat: Solve the Dream Team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+//  throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+   if (Array.isArray(members)) {
+      let dreamTeam = '';
+      members.forEach(member => {
+         if (typeof member === 'string') {
+            member = member.replace(/\s+/g, '');
+            dreamTeam = dreamTeam + member[0];
+         }
+      });
+      dreamTeam = dreamTeam.toUpperCase();
+      dreamTeam = dreamTeam.split('').sort().join('');
+      return dreamTeam ? dreamTeam : false;
+   } else {
+      return false;
+   }
 }
 
 module.exports = {
